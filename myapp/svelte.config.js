0ebaf7a304e50075e2fd965ad05987b398d3be87
Ainaa <admin@ainaarawaida.com>
2023-05-03 22:56:33 +0800

    1

--- myapp/svelte.config.js
+++ myapp/svelte.config.js
@@ -3,7 +3,6 @@ const dev = process.argv.includes('dev');
 import { defineConfig, loadEnv } from 'vite'
 
 let dotenv = loadEnv('', process.cwd());
-// console.log(".env", dotenv?.VITE_MYASSETSURL)
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,10 +10,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
 		paths: {
 			assets: dev ? '' : `${dotenv?.VITE_MYASSETSURL}`,
-			base: dev ? '' : `${dotenv?.VITE_MYBASEURL}`,
+			base: dev ? '' : `${dotenv?.VITE_MYBASE}`,
 		}
 	}
 };
